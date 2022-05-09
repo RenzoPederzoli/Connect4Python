73977cae5a42a8fc7d36c9f9bcfad16cb82def1f
SKalltheway <samkaczynski@gmail.com>
2022-05-09 16:46:51 -0400

    addedmenuscreen

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ BLUE = (0,0,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 YELLOW = (255,255,0)
+mainClock = pygame.time.Clock()
+
+from pygame.locals import *
+pygame.init()
  
 ROW_COUNT = 6
 COLUMN_COUNT = 7
@@ -89,64 +93,115 @@ size = (width, height)
 RADIUS = int(SQUARESIZE/2 - 5)
  
 screen = pygame.display.set_mode(size)
+
 #Calling function draw_board again
 draw_board(board)
 pygame.display.update()
  
 myfont = pygame.font.SysFont("monospace", 75)
  
-while not game_over:
+
+pygame.display.set_caption('game base')
+screen = pygame.display.set_mode((500, 500),0,32)
  
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+font = pygame.font.SysFont(None, 20)
  
-        if event.type == pygame.MOUSEMOTION:
-            pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
-            posx = event.pos[0]
-            if turn == 0:
-                pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
-            else: 
-                pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+ 
+click = False
+ 
+def main_menu():
+    while True:
+        screen = pygame.display.set_mode(size)
+        screen.fill((0,0,0))
+        draw_text('Click Red Square to Join Game', font, (255, 255, 255), screen, 50, 70)
+ 
+        mx, my = pygame.mouse.get_pos()
+ 
+        button_1 = pygame.Rect(50, 100, 200, 50)
+        if button_1.collidepoint((mx, my)):
+            if click:
+                gameplay()
+        pygame.draw.rect(screen, (255, 0, 0), button_1)
+ 
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+ 
         pygame.display.update()
- 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
-            #print(event.pos)
-            # Ask for Player 1 Input
-            if turn == 0:
+        mainClock.tick(60)
+
+
+def gameplay():
+    game_over = False
+    turn = 0
+    while not game_over:
+    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
                 posx = event.pos[0]
-                col = int(math.floor(posx/SQUARESIZE))
- 
-                if is_valid_location(board, col):
-                    row = get_next_open_row(board, col)
-                    drop_piece(board, row, col, 1)
- 
-                    if winning_move(board, 1):
-                        label = myfont.render("Player 1 wins!!", 1, RED)
-                        screen.blit(label, (40,10))
-                        game_over = True
- 
- 
-            # # Ask for Player 2 Input
-            else:               
-                posx = event.pos[0]
-                col = int(math.floor(posx/SQUARESIZE))
- 
-                if is_valid_location(board, col):
-                    row = get_next_open_row(board, col)
-                    drop_piece(board, row, col, 2)
- 
-                    if winning_move(board, 2):
-                        label = myfont.render("Player 2 wins!!", 1, YELLOW)
-                        screen.blit(label, (40,10))
-                        game_over = True
- 
-            print_board(board)
-            draw_board(board)
- 
-            turn += 1
-            turn = turn % 2
- 
-            if game_over:
-                pygame.time.wait(3000)
+                if turn == 0:
+                    pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+                else: 
+                    pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+            pygame.display.update()
+    
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+                #print(event.pos)
+                # Ask for Player 1 Input
+                if turn == 0:
+                    posx = event.pos[0]
+                    col = int(math.floor(posx/SQUARESIZE))
+    
+                    if is_valid_location(board, col):
+                        row = get_next_open_row(board, col)
+                        drop_piece(board, row, col, 1)
+    
+                        if winning_move(board, 1):
+                            label = myfont.render("Player 1 wins!!", 1, RED)
+                            screen.blit(label, (40,10))
+                            game_over = True
+    
+    
+                # # Ask for Player 2 Input
+                else:               
+                    posx = event.pos[0]
+                    col = int(math.floor(posx/SQUARESIZE))
+    
+                    if is_valid_location(board, col):
+                        row = get_next_open_row(board, col)
+                        drop_piece(board, row, col, 2)
+    
+                        if winning_move(board, 2):
+                            label = myfont.render("Player 2 wins!!", 1, YELLOW)
+                            screen.blit(label, (40,10))
+                            game_over = True
+    
+                print_board(board)
+                draw_board(board)
+    
+                turn += 1
+                turn = turn % 2
+    
+                if game_over:
+                    pygame.time.wait(3000)
+main_menu()
